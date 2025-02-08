8af376ea3a532cb464200939489f2e4ed68022a4
Ayoub <ayoubbarahal2@gmail.com>
2025-02-08 16:11:40 +0100

    vercel deploy version 3

--- mon-projet/src/index.js
+++ mon-projet/src/index.js
@@ -12,7 +12,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="*" element={<Games />} />
+        <Route path="/games" element={<Games />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
